e34d78d0ffc6e66d479b1146444c5aa15dee8ce3
SunthornPuu <phakinphakin9@gmail.com>
2026-04-27 21:33:47 +0700

    contruct part implemented

--- decision_tree.cpp
+++ decision_tree.cpp
@@ -12,7 +12,8 @@ struct Node{
         double value = -1;
         Node* left = nullptr;
         Node* right = nullptr;
-        int property_index,property_value;
+        int property_index = -1;
+        double property_value = -1;
 };
 
 class DecisionTree{
@@ -23,19 +24,52 @@ class DecisionTree{
                 vector<double> y;
                 int M_depth,m_splits;
                 Node root;
-                void construct(Node node, int l, int r,int depth){
+                int resort(int l,int r,int feature_index,double feature_value ){
+                        vector<pair<double,vector<double>>> resorted(r-l);
+                        for(int i=l;i<r;i++)resorted[i-l]=  mp(X[i][feature_index],X[i]);
+                        sort(resorted.begin(),resorted.end());                              
+                }
+
+                void construct(Node &node, int l, int r,int depth){
                         int val = 0;
                         for(int i=l;i<r;i++)val += double(y[i]);
                         val /= double(r-l);
+                        node.value = val;
                         if(depth>=M_depth)return;
                         Node left,right;
-                        dd best_split = mp(0,0);
+                        id best_split = mp(0,0);
                         double most_reduction = INT_MAX;
                         for(int m=0;m<M;m++){
-                                vector<id> result(N);
-                                for(int i=0;i<N;i++)result[i]=mp(X[m][i],y[i]);
-                                sort(result.begin(),result.end());
+                                for(int i=l;i<r;i++){
+                                        //first calculate the mean of group 1 nad 2
+                                        //noted that group1 has x less than the set one
+                                        double mean1 = 0, mean2 = 0;
+                                        int c1 = 0, c2 = 0;
+                                        for(int j=l;j<r;j++){
+                                                if(X[j][m]<X[i][m])c1++,mean1+=y[j];
+                                                else c2++,mean2+=y[j];
+                                        }
+                                        mean1/=double(c1);
+                                        mean2/=double(c2);
+                                        if(c1<m_splits||c2<m_splits)continue;
+                                        double var1=0,var2=0;
+                                        for(int j=l;j<r;j++){  
+                                                if(X[j][m]<=X[i][m])var1+=pow(y[j]-var1,2);
+                                                else var2+=pow(y[j]-var2,2);
+                                        }
+                                        if(max(var1,var2)<=most_reduction)
+                                                best_split = mp(m,X[i][m]), most_reduction = max(var1,var2);
+                                }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
                         }
+                        if(best_split.f==-1)return;
+                        int split_index = resort(l,r,best_split.f,best_split.s);
+                        construct(left,l,split_index,depth+1);
+                        construct(right,split_index,r,depth+1);
+                        node.left = &left;
+                        node.right = &right;
+                        node.property_index = best_split.f;
+                        node.property_value = best_split.s;
+
                 }
         public:
                 DecisionTree(vector<vector<double>> given_set, int max_depth = 999999, int min_samples_split = 1){
---
+++ formula.cpp
@@ -0,0 +1,6 @@
+long unsigned freakyAssVariable;
+
+// What can freakyAssVariable store?
+
+std::pair<long, unsigned> OhMyGod;
+OhMyGod.first = 545454;
